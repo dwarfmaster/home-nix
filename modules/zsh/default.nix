4f0b712e845a4aa7ca07c8a7374a6e359dadbd4f
DwarfMaster <luc@dwarfmaster.net>
2019-08-27 23:24:32 +0200

    Tweak spaceship-prompt coloration to go with ST's

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -53,18 +53,20 @@ let lib = import ../../lib.nix; in
     #  ___) | |_) | (_| | (_|  __/___) | | | | | |_) |
     # |____/| .__/ \__,_|\___\___|____/|_| |_|_| .__/ 
     #       |_|                                |_|    
-    SPACESHIP_PROMPT_ORDER  = [ "dir" "git" "ruby" "rust" "haskell" "julia" "line_sep" "vi_mode" "jobs" "exit_code" "char" ];
+    SPACESHIP_PROMPT_ORDER  = [ "dir" "git" "ruby" "rust" "haskell" "julia"
+                                "line_sep" "vi_mode" "jobs" "exit_code" "char" ];
     SPACESHIP_RPROMPT_ORDER = [ "exec_time" "battery" "user" "host" "time" ];
     SPACESHIP_PROMPT_ADD_NEWLINE       = true;
     SPACESHIP_PROMPT_SEPARATE_LINE     = true;
     SPACESHIP_PROMPT_FIRST_PREFIX_SHOW = false;
     SPACESHIP_PROMPT_PREFIXES_SHOW     = true;
     SPACESHIP_PROMPT_SUFFIXES_SHOW     = true;
+    SPACESHIP_CHAR_COLOR_SUCCESS       = "cyan";
+    SPACESHIP_CHAR_COLOR_SECONDARY     = "magenta";
 
     SPACESHIP_TIME_SHOW        = true;
+    SPACESHIP_TIME_COLOR       = "cyan";
     SPACESHIP_TIME_FORMAT      = "%* [%D]";
-    SPACESHIP_USER_SHOW        = true;
-    SPACESHIP_HOST_SHOW        = true;
     SPACESHIP_DIR_SHOW         = true;
     SPACESHIP_DIR_TRUNC        = 4;
     SPACESHIP_DIR_TRUNC_PREFIX = ".../";
@@ -77,6 +79,7 @@ let lib = import ../../lib.nix; in
     SPACESHIP_HASKELL_SHOW     = true;
     SPACESHIP_JULIA_SHOW       = true;
     SPACESHIP_EXEC_TIME_SHOW   = true;
+    SPACESHIP_EXEC_TIME_COLOR  = "red";
     SPACESHIP_BATTERY_SHOW     = "always";
     SPACESHIP_VI_MODE_SHOW     = true;
     SPACESHIP_JOBS_SHOW        = true;
