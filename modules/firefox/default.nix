d9a3df1178a61840c26ebef374fe80eb182df9e4
DwarfMaster <luc@dwarfmaster.net>
2019-08-31 15:24:54 +0200

    Try (and fail) to make rofi-tab-switcher work

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -1,9 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
-{
+let
+  lib   = import ../../lib.nix;
+  mpkgs = import ./packages.nix { inherit pkgs; };
+in {
   programs.firefox = {
     enable = true;
     enableIcedTea = true;
+  };
+
+  home.file.".mozilla/native-messaging-hosts/rofi_interface.json".text = lib.toJSON {
+    name               = "rofi_interface";
+    description        = "Native backend for rofi tab switcher.";
+    path               = "${mpkgs.rofi-tab-switcher}/bin/rofiface.py";
+    type               = "stdio";
+    allowed-extensions = [ "@rofi.tab.switcher" ];
   };
 }
 
