33c5e7c4edf0847a2386803f5520f71291725f97
DwarfMaster <luc@dwarfmaster.net>
2019-10-27 01:18:04 +0200

    Use lastest version of home-manager

--- home/modules/home-manager/default.nix
+++ home/modules/home-manager/default.nix
@@ -1,9 +1,15 @@
 { ... }:
 
-{
-  programs.home-manager = {
-    enable = true;
-    path = https://github.com/rycee/home-manager/archive/release-19.03.tar.gz;
-  };
+let
+
+  home-manager-pkgs = import <home-manager> { };
+
+in {
+  packages = [ home-manager-pkgs.home-manager ];
+
+  # programs.home-manager = {
+  #   enable = true;
+  #   path = https://github.com/rycee/home-manager/archive/release-19.03.tar.gz;
+  # };
 }
 
