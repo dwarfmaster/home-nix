56df3dd1909373c2a1ce660b261eb0bdf87602da
DwarfMaster <luc@dwarfmaster.net>
2019-10-28 16:30:34 +1000

    Improve taskwarrior configuration

--- home/modules/taskwarrior/default.nix
+++ home/modules/taskwarrior/default.nix
@@ -6,6 +6,9 @@
     colorTheme = ./color.theme;
     config = {
       default.command = "ready";
+      report.ready = {
+        filter = "+READY project.not:Irl.improvment.bugs";
+      };
     };
   };
 }
