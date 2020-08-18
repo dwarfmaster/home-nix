13170c26edf9f5bac0ad42436546f82a8dcaa59b
DwarfMaster <luc@dwarfmaster.net>
2020-08-18 12:26:52 +0200

    Add direnv support for nix flakes

--- home/modules/direnv/default.nix
+++ home/modules/direnv/default.nix
@@ -9,8 +9,14 @@ in {
     enableBashIntegration = true;
     enableZshIntegration = true;
     stdlib = ''
-pwd_hash=$(echo -n $PWD | ${pkgs.perl}/bin/shasum | cut -d ' ' -f 1
+pwd_hash=$(echo -n $PWD | ${pkgs.perl}/bin/shasum | cut -d ' ' -f 1)
 direnv_layout_dir=$XDG_CACHE_HOME/direnv/layouts/$pwd_hash
+
+use_flake() {
+  watch_file flake.nix
+  watch_file flake.lock
+  eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
+}
 '';
   };
 }
