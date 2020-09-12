50620621d819fe5503879053c7c175bfde8e6e1f
DwarfMaster <luc@dwarfmaster.net>
2020-09-12 15:19:23 +0200

    Configure ledger-mode to work with hledger

--- home/modules/emacs/config.el
+++ home/modules/emacs/config.el
@@ -721,3 +721,16 @@ Project switcher
          (summary (dwarfmaster/elfeed/webcomics-summary tag)))
     (insert summary)
     (org-table-align)))
+
+;; Language: ledger
+ ;; _             _
+;; | |    ___  __| | __ _  ___ _ __
+;; | |   / _ \/ _` |/ _` |/ _ \ '__|
+;; | |__|  __/ (_| | (_| |  __/ |
+;; |_____\___|\__,_|\__, |\___|_|
+                 ;; |___/
+(add-to-list 'auto-mode-alist '("\\.journal\\'" . ledger-mode))
+;; Configure ledger-mode to work with hledger
+(setq ledger-mode-should-check-version nil
+      ledger-report-links-in-register nil
+      ledger-binary-path "hledger")
