84e203f2166739bf9c2dd0abac439e9531734fd8
DwarfMaster <luc@dwarfmaster.net>
2021-02-03 18:40:30 +0100

    Began configuring ssh server for dwarfmaster.net

--- hosts/dwarfmaster.nix
+++ hosts/dwarfmaster.nix
@@ -76,4 +76,22 @@
       };
     };
   };
+
+  services.openssh = {
+    enable = true;
+    banner = ''
+ ____                      __ __  __           _
+|  _ \__      ____ _ _ __ / _|  \/  | __ _ ___| |_ ___ _ __
+| | | \ \ /\ / / _` | '__| |_| |\/| |/ _` / __| __/ _ \ '__|
+| |_| |\ V  V / (_| | |  |  _| |  | | (_| \__ \ ||  __/ |
+|____/  \_/\_/ \__,_|_|  |_| |_|  |_|\__,_|___/\__\___|_|
+
+Welcome !
+'';
+    forwardX11 = false;
+    logLevel = "VERBOSE";
+    # passwordAuthentification = false;
+    # permitRootLogin = false;
+    ports = [ 2222 ];
+  };
 }
--- users/luc-server/default.nix
+++ users/luc-server/default.nix
@@ -3,6 +3,10 @@
     ../luc-common
   ];
 
+  users.users.luc.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDePPhrDtSu9mtHKdCgMDEGuWak40+a2r2IfW+mzUEeV luc@tungdil"
+  ];
+
   # TODO
   home-manager.users.luc = {
     imports = [
