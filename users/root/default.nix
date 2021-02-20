0f2ff65335dd52a38fd0ce5b9c27e0b3fcaf46e1
DwarfMaster <luc@dwarfmaster.net>
2021-02-20 17:59:54 +0100

    Makes sure to always have the root password set

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,3 +1,14 @@
-{
-  users.users.root.hashedPassword = import ./password.nix;
+{ pkgs, config, ... }:
+
+let
+
+  passwd = import ./password.nix;
+
+in {
+  users.users.root.hashedPassword = passwd;
+
+  assertions = [{
+    assertion = passwd != "";
+    message = "root password must be set !";
+  }];
 }
