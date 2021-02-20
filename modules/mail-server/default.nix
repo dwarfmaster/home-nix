42eed9ce8d8a2911607adb6840f0a663d463be10
DwarfMaster <luc@dwarfmaster.net>
2021-02-20 18:34:26 +0100

    Finalize mail server

--- modules/mail-server/default.nix
+++ modules/mail-server/default.nix
@@ -15,13 +15,12 @@ args@{ config, lib, pkgs, ... }:
       };
     };
 
-    # TODO after I update the DNS to the new server
-    # certificateScheme = 3;
+    certificateScheme = 3;
 
     enableImap = true;
     enablePop3 = true;
-    # enableImapSsl = true;
-    # enablePop3Ssl = true;
+    enableImapSsl = true;
+    enablePop3Ssl = true;
 
     # We don't use server-side sieves, all the filtering will be done client-side
     enableManageSieve = false;
