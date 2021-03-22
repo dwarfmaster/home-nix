026c07f9fd4a2ee2d7f5b45e3e211b26301264f2
DwarfMaster <luc@dwarfmaster.net>
2021-03-22 20:39:42 +0100

    Install geekie

--- users/profiles/photos/default.nix
+++ users/profiles/photos/default.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs; [
     darktable   # Image manager
+    geeqie      # Image manager
     sxiv        # Lightweight X image viewer
     imagemagick # Convert any image format to any other
   ];
