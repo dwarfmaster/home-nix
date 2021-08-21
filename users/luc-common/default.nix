a52b8bc1bf590136dad18407c1f705677238ce8f
DwarfMaster <luc@dwarfmaster.net>
2021-08-21 11:41:32 +0200

    Install tree on varracs luc

--- users/luc-common/default.nix
+++ users/luc-common/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   users.users.luc = {
     uid = 1000;
@@ -27,6 +29,9 @@
     xdg.enable = true;
     programs.git.userName = "DwarfMaster";
     programs.git.userEmail = "luc@dwarfmaster.net";
+
+    # TODO factor common packages somewhere
+    home.packages = with pkgs; [ tree ];
 
     manual = {
       html.enable = true;
