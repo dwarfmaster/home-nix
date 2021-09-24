8f9553d01e7889e8482da76bfd21d660b3c2dee5
DwarfMaster <luc@dwarfmaster.net>
2021-09-24 12:53:35 +0200

    Improve screen locking

--- user/interface/locking/default.nix
+++ user/interface/locking/default.nix
@@ -7,20 +7,41 @@ let
   physlock = "${pkgs.physlock}/bin/physlock";
   i3lock = "${pkgs.i3lock}/bin/i3lock";
   vlock = "${pkgs.vlock}/bin/vlock";
+  setxkbmap = "${pkgs.xorg.setxkbmap}/bin/setxkbmap";
 
-  locker = lib.writeShellScript "lock-script"
+  # Make it a program somewhere else
+  unsetxkbmap = pkgs.writeScript "unsetxkbmap"
     ''
-    case $(tty) in
-      /dev/tty*)
-        ${vlock}
-      ;;
-      *)
-        ${physlock} -l
-        ${i3lock}
-        ${physlock} -L
-      ;;
-    esac
+    #!${config.programs.nushell.package}/bin/nu
+
+    let toremove = $nu.env.XKBOPTION
+
+    # Get all options
+    let options = (setxkbmap -query | lines | split column ': ' | str trim | pivot -ri | get options | split row ',')
+
+    # Unset all options
+    ${setxkbmap} -option
+
+    # Reset all other options
+    echo $options | where $it !~ $toremove | each { ${setxkbmap} -option $it }
+    '';
+
+  locker = pkgs.writeShellScript "lock-xsession"
+    ''
+      # Disable switching to VTs
+      ${setxkbmap} -option srvrkeys:none
+      # Lock X11 screen
+      ${i3lock} -n
+      # Re-enable switching to VTs
+      XKBOPTION=srvrkeys:none ${unsetxkbmap}
     '';
 in {
   home.packages = [ pkgs.physlock pkgs.vlock pkgs.i3lock ];
+  programs.zsh.shellAliases = {
+    xlock = "${locker}";
+    unsetx = "${unsetxkbmap}";
+  };
+  programs.bash.shellAliases = {
+    xlock = "${locker}";
+  };
 }
