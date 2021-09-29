c76de6be999ea2f7027243026d697333ca13e61d
DwarfMaster <luc@dwarfmaster.net>
2021-09-29 17:42:03 +0200

    Safer screen locking

--- user/interface/locking/default.nix
+++ user/interface/locking/default.nix
@@ -1,6 +1,5 @@
 { config, lib, ... }:
 
-# TODO setting locking by session on loginctl event
 let
   inherit (config.pkgsets) pkgs;
 
@@ -8,41 +7,86 @@ let
   i3lock = builtins.replaceStrings ["%%"] ["%"] "${config.services.screen-locker.lockCmd}";
   vlock = "${pkgs.vlock}/bin/vlock";
   setxkbmap = "${pkgs.xorg.setxkbmap}/bin/setxkbmap";
+  bctl = "${pkgs.brightnessctl}/bin/brightnessctl";
 
-  # Make it a program somewhere else
+  # TODO Make it a program somewhere else
   unsetxkbmap = pkgs.writeScript "unsetxkbmap"
     ''
-    #!${config.programs.nushell.package}/bin/nu
+      #!${config.programs.nushell.package}/bin/nu
 
-    let toremove = $nu.env.XKBOPTION
-
-    # Get all options
-    let options = (setxkbmap -query | lines | split column ': ' | str trim | pivot -ri | get options | split row ',')
-
-    # Unset all options
-    ${setxkbmap} -option
-
-    # Reset all other options
-    echo $options | where $it !~ $toremove | each { ${setxkbmap} -option $it }
+      let toremove = $nu.env.XKBOPTION
+      # Get all options
+      let options = (${setxkbmap} -query | lines | split column ': ' | str trim | pivot -ri | get options | split row ',')
+      # Unset all options
+      ${setxkbmap} -option
+      # Reset all other options
+      echo $options | where $it !~ $toremove | each { ${setxkbmap} -option $it }
     '';
 
-  locker = pkgs.writeShellScript "lock-xsession"
+  dim-screen = pkgs.writeShellScriptBin "dim_screen"
     ''
+      BRIGHTNESS=$(${bctl} --class=backlight get)
+      trap "${bctl} --class=backlight set $BRIGHTNESS; exit 1" SIGINT SIGTERM
+      ${bctl} --class=backlight set 5%
+      while true; do
+        sleep 1
+      done
+    '';
+
+  locker = pkgs.writeShellScriptBin "lock-xsession"
+    ''
+      # Activate systemctl target
+      systemctl --user start xsession-lock.target
       # Disable switching to VTs
       ${setxkbmap} -option srvrkeys:none
       # Lock X11 screen
       ${i3lock} -n
       # Re-enable switching to VTs
       XKBOPTION=srvrkeys:none ${unsetxkbmap}
+      # De-activate systemctl target
+      systemctl --user stop xsession-lock.target
     '';
+
 in {
   services.screen-locker.enableBase16Theme = true;
-  home.packages = [ pkgs.physlock pkgs.vlock pkgs.i3lock ];
-  programs.zsh.shellAliases = {
-    xlock = "${locker}";
-    unsetx = "${unsetxkbmap}";
+  home.packages = [ locker dim-screen pkgs.vlock pkgs.brightnessctl ];
+
+  systemd.user.services = {
+    dpms-setup = {
+      Unit = {
+        Description = "Set up DPMS configuration for X11 session";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.xorg.xset}/bin/xset s 300 60";
+      };
+    };
+    xsession-locker-setup = {
+      Unit = {
+        Description = "Screen locker";
+        After = [ "graphical-session-pre.target" "dpms-setup.service" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.xss-lock}/bin/xss-lock -s $XDG_SESSION_ID -n ${dim-screen}/bin/dim_screen -- ${locker}/bin/lock-xsession";
+      };
+    };
   };
-  programs.bash.shellAliases = {
-    xlock = "${locker}";
+  systemd.user.targets = {
+    xsession-lock = {
+      Unit = {
+        Description = "Target active when the X11 session is locked";
+      };
+    };
   };
 }
