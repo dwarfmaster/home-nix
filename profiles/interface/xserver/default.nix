c3c931a4c981d189eca0909827cc88febc394b14
DwarfMaster <luc@dwarfmaster.net>
2021-09-29 11:26:58 +0200

    Cleanup system x11 config

--- profiles/interface/xserver/default.nix
+++ profiles/interface/xserver/default.nix
@@ -1,17 +1,19 @@
 { config, lib, pkgs, ... }:
 
 {
+  # By default, no window manager nor DE is installed, and the display manager
+  # only allows to log in  to xterm. The display manager installed is lightdm.
+  # By setting startx as the display manager, I can run the graphic session
+  # with startx.
   services.xserver = {
     enable = true;
-    layout = "fr";
+    autorun = false;
+    displayManager.startx.enable = true;
 
-    # Use startx as display-manager
-    displayManager = {
-      startx.enable = true;
-      defaultSession = "none+xmonad";
-    };
-    desktopManager.xterm.enable = false;
-    windowManager.xmonad.enable = true;
+    enableTCP = false;
+    enableCtrlAltBackspace = true;
+
+    layout = "fr";
 
     # Support for touchpad
     libinput.enable = true;
