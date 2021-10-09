0aaac9d51ef38b4f5dca8c7a87cc8db7200a6505
DwarfMaster <luc@dwarfmaster.net>
2021-10-09 19:09:27 +0200

    Move from pulseaudio to pipewire

--- profiles/interface/sound/default.nix
+++ profiles/interface/sound/default.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-  sound = {
+  # Conflicts with pipewire
+  sound.enable = false;
+  # TODO find precisely what it does
+  security.rtkit.enable = true;
+  # Enable and setup PipeWire
+  services.pipewire = {
     enable = true;
-    enableOSSEmulation = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Default session manager
+    media-session.enable = true;
   };
-  hardware.pulseaudio.enable = true;
 }
--- user/programs/audio/default.nix
+++ user/programs/audio/default.nix
@@ -1,11 +1,18 @@
 { config, ... }:
 
 let
-  inherit (config.pkgsets) pkgs;
+  inherit (config.pkgsets) pkgs unstable;
 in {
   home.packages = builtins.attrValues {
     inherit (pkgs)
-      audacity # sound and music editor
-      ;
+      audacity     # sound and music editor
+      pipewire     # For the pipewire CLI tools
+      pulseaudio   # For the pulseaudio CLI tools
+      pavucontrol  # CLI sound interface
+    ;
+    inherit (unstable)
+      helvum      # GUI for pipewire
+      easyeffects # GUI for effects on pipewire
+    ;
   };
 }
