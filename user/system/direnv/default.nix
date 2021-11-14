18c6236369c0e68a87b55214fc311f912e7398e5
DwarfMaster <luc@dwarfmaster.net>
2021-11-14 15:28:17 +0100

    Fix direnv flake support

--- user/system/direnv/default.nix
+++ user/system/direnv/default.nix
@@ -4,20 +4,9 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    nix-direnv.enableFlakes = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
-    stdlib = ''
-direnv_layout_dir() {
-  pwd_hash=$(echo -n $PWD | ${pkgs.perl}/bin/shasum | cut -d ' ' -f 1)
-  echo "$XDG_CACHE_HOME/direnv/layouts/$pwd_hash"
-}
-
-use_flake() {
-  watch_file flake.nix
-  watch_file flake.lock
-  eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
-}
-'';
   };
 
   programs.doom = {
