d16b0d8ce495d28c1cd58f22f3e0f18c25772d91
DwarfMaster <luc@dwarfmaster.net>
2021-11-29 13:08:30 +0100

    Add gmp to pkg config so that opam can install coq

--- user/languages/coq/default.nix
+++ user/languages/coq/default.nix
@@ -1,6 +1,14 @@
 { config, lib, ... }:
 
-{
-  home.packages = [ config.pkgsets.pkgs.coq ];
+let
+  inherit (config.pkgsets) pkgs;
+in {
+  home.packages = builtins.attrValues {
+    inherit (pkgs)
+      coq;
+  };
   programs.doom.initModules.lang = [ "coq" ];
+  # I need to tell opam where to find gmp
+  pkgconfig.enable = true;
+  pkgconfig.path = [ "${pkgs.gmp.dev}/lib/pkgconfig" ];
 }
