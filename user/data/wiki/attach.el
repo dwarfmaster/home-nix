7a1259b6ad354ebda96271a799fb3d6098aed4cb
DwarfMaster <luc@dwarfmaster.net>
2022-01-30 19:21:17 +0100

    Add better org attachment resolution function

--- user/data/wiki/attach.el
+++ user/data/wiki/attach.el
@@ -6,6 +6,40 @@
 ;; | |_| | | | (_| |  / ___ \ |_| || (_| | (__| | | |
 ;;  \___/|_|  \__, | /_/   \_\__|\__\__,_|\___|_| |_|
 ;;            |___/
+
+;; I'm using my own system where attachment links are resolved by moving up the
+;; tree in when resolved.
+(defun dwarfmaster/attach-list-candidates ()
+  "List all attachments available for current heading."
+  (save-excursion
+    (let ((files '()))
+      (outline-back-to-heading)
+      (while (not (org-before-first-heading-p))
+        (if (and (org-id-get) (file-exists-p (org-attach-dir-from-id (org-id-get))))
+            (let* ((dir (org-attach-dir-from-id (org-id-get)))
+                   (locals (directory-files dir)))
+              (setq files (cl-merge 'list files locals #'string-lessp))))
+        (outline-up-heading 1))
+      files)))
+;; TODO better complete solution for attach links
+
+(defun dwarfmaster/attach-resolve-link (file)
+  "Look for FILE in attach directories."
+  (save-excursion
+    (outline-back-to-heading)
+    (let ((path nil))
+      (while (and (not path) (not (org-before-first-heading-p)))
+        (if (and (org-id-get)
+                 (file-exists-p (concat (org-attach-dir-from-id (org-id-get)) "/" file)))
+            (setq path (concat (org-attach-dir-from-id (org-id-get)) "/" file)))
+        (outline-up-heading 1))
+      path)))
+
+(defun dwarfmaster/attach-follow (file &optional arg)
+  "Open FILE attachment.
+See `org-open-file' for details about ARG"
+  (org-link-open-as-file (dwarfmaster/attach-resolve-link file) arg))
+
 (after! org
   (setq org-attach-directory "/data/luc/annex/data")
   (setq org-attach-method 'mv)
@@ -17,3 +51,5 @@
   (setq org-attach-annex-auto-get 'ask)
   ;; Do not commit attachements with git ! Will use a special hook
   (setq org-attach-commit nil))
+(after! org-attach
+  (org-link-set-parameters "attachment" :follow #'dwarfmaster/attach-follow))
