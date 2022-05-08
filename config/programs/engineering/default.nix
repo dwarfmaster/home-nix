0a3e1490c156fad42bd6285e580adff217fdd196
DwarfMaster <luc@dwarfmaster.net>
2022-05-08 09:23:46 +0200

    Install misc engineering softwares

--- config/programs/engineering/default.nix
+++ config/programs/engineering/default.nix
@@ -1,14 +1,30 @@
 { config, pkgs, ... }:
 
-{
+let
+  implicitcad = pkgs.haskellPackages.implicit;
+in {
   home.packages = builtins.attrValues {
+    inherit implicitcad; # haskell-based CAD
     inherit (pkgs)
       # 3D
-      solvespace # generic CAD
-      freecad    # generic CAD
+      solvespace  # minimalistic CAD
+      freecad     # generic CAD
+      openscad    # text-based CAD
+
+      # Elec
+      kicad       # PCB prototyping and design
+      ngspice     # Electronic circuits simulator
+      horizon-eda # PCB prototyping and design
+      librepcb    # PCB prototyping and design
+
+      # GCode
+      slic3r      # GCode generator for 3D printers
+      # TODO dxf2gcode  # gcode generator for CNC
+      # TODO camotics   # GCode viewer/CNC simulator
 
       # Misc
-      leocad     # LEGO models designer
-      ;
+      leocad      # LEGO models designer
+      libxslt     # Tools to work with xsl files, used by kicad
+    ;
   };
 }
