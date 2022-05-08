c7bf506d93a591d3b38a2e9a020d972302f230fa
DwarfMaster <luc@dwarfmaster.net>
2022-05-08 09:23:21 +0200

    Install tokei line counter software

--- config/languages/tools/default.nix
+++ config/languages/tools/default.nix
@@ -3,16 +3,18 @@
 {
   home.packages = builtins.attrValues {
     inherit (pkgs)
-      pijul    # VCS based on sound theory
-      gnumake  # Simple generic purpose build system
-      ninja    # Simple generic purpose build system
-      cmake    # Generic purpose build system
-      cloc     # Count code lines
-      valgrind # Generic purpose debugger
-      gnum4    # Macro preprocessor
-      ctags    # Objects indexer for many languages
-      autoconf # Makefile generator
-      automake # Same
+      pijul     # VCS based on sound theory
+      gnumake   # Simple generic purpose build system
+      ninja     # Simple generic purpose build system
+      cmake     # Generic purpose build system
+      cloc      # Count code lines
+      tokei     # Better cloc
+      # tokei-pie # Pie representation of tokei TODO
+      valgrind  # Generic purpose debugger
+      gnum4     # Macro preprocessor
+      ctags     # Objects indexer for many languages
+      autoconf  # Makefile generator
+      automake  # Same
       ;
     perf = pkgs.linuxPackages.perf; # Profile programs
   };
