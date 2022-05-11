883cd00781aa8141ec63130675bee3e2f9562ced
DwarfMaster <luc@dwarfmaster.net>
2022-05-11 15:02:43 +0200

    zathura minimal configuration

--- config/programs/documents/default.nix
+++ config/programs/documents/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
-{
+let
+  copier = pkgs.writeScript "xcopier" ''
+    echo "$1" | ${pkgs.xclip}/bin/xclip -i
+  '';
+in {
   home.packages = [
     pkgs.libsForQt5.okular # Heavyweight pdf reader
   ] ++ builtins.attrValues {
@@ -18,4 +22,14 @@
       fanficfare  # Download and convert to epub fanfiction from the web
       ;
   };
+  xdg.configFile."zathura/zathurarc".text = ''
+    map u exec ${pkgs.reupload}/bin/reupload\ "$FILE"
+    map C exec ${copier}\ "$FILE"
+
+    set database null
+    # For SyncTex
+    set dbus-service true
+    set dbus-raise-window true
+    set synctex true
+  '';
 }
