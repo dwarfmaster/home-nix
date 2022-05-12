ffda6f0c33ebecd886b113cb1022de376efffc9f
DwarfMaster <luc@dwarfmaster.net>
2022-05-12 23:34:37 +0200

    Fix coq hott installation

--- packages/default.nix
+++ packages/default.nix
@@ -10,6 +10,6 @@ in {
   coq-hott_8_12 = let cpkgs = pkgs.coqPackages_8_12; in cpkgs.callPackage ./coq-hott { };
   coq-hott_8_13 = let cpkgs = pkgs.coqPackages_8_13; in cpkgs.callPackage ./coq-hott { };
   coq-hott_8_14 = let cpkgs = pkgs.coqPackages_8_14; in cpkgs.callPackage ./coq-hott { };
-  coq-hott_8_15 = let cpkgs = pkgs.coqPackages_8_15; in cpkgs.callPackage ./coq-hott { };
-  coq-hott      = pkgs.callPackage ./coq-hott { };
+  # coq-hott_8_15 = let cpkgs = pkgs.coqPackages_8_15; in cpkgs.callPackage ./coq-hott { };
+  coq-hott      = pkgs.coqPackages.callPackage ./coq-hott { };
 }
--- profiles/hm/languages/coq/default.nix
+++ profiles/hm/languages/coq/default.nix
@@ -1,14 +1,13 @@
 { config, lib, pkgs, ... }:
 
 let
-  packages = pkgs.unstable.coqPackages_8_15;
+  packages = pkgs.coqPackages_8_14;
 in {
   programs.coq = {
     enable = true;
     inherit packages;
     libraries = [
-      # TODO fix
-      # (packages.callPackage ../../../packages/coq-hott { })
+      pkgs.coq-hott_8_14
     ];
   };
   programs.doom-emacs.config.initModules.lang = [ "coq" ];
