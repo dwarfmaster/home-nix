b8ba4a23bf585526026a75bcb54b87a4a7adbf53
DwarfMaster <luc@dwarfmaster.net>
2022-06-25 15:23:44 +0200

    Install scan-build

--- profiles/hm/languages/cpp/default.nix
+++ profiles/hm/languages/cpp/default.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
-{
+let
+scan-build = pkgs.python310Packages.callPackage ./scan-build.nix {};
+in {
   home.packages = builtins.attrValues {
     inherit (pkgs)
       gcc              # C/C++ compiler
@@ -10,6 +12,9 @@
       ccls             # LSP server for CPP
       bazel            # Build-system by google
       bazel-buildtools # buildifier, buildozer and unused_deps
+      ;
+    inherit
+      scan-build       # Creates command database
       ;
   };
 
@@ -21,7 +26,7 @@
       config.source = ./bazel.el;
       packages.text = ''
         (package! bazel)
-      '';
+        '';
       nix = {
         bazel = "${pkgs.bazel}/bin/bazel";
         buildifier = "${pkgs.bazel-buildtools}/bin/buildifier";
@@ -31,7 +36,7 @@
   };
 
   programs.cookiecutter.templates = {
-    # TODO add CCLS support to template
+# TODO add CCLS support to template
     cpp = ./cmake;
   };
 }
---
+++ profiles/hm/languages/cpp/scan-build.nix
@@ -0,0 +1,27 @@
+{ buildPythonPackage, fetchPypi, lib }:
+
+let
+pname = "scan-build";
+version = "2.0.20";
+in buildPythonPackage {
+  inherit pname version;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "f1f9f1dc3daf906ef106077dbac4d872f5740843173dc74070ef3b39da3d0f07";
+  };
+
+  meta = with lib; {
+    description = "Creates a compilation database by intercepting call to compiler";
+    longDescription = ''
+      A package designed to wrap a build so that all calls to gcc/clang are
+      intercepted and logged into a compilation database and/or piped to the clang
+      static analyzer. Includes intercept-build tool, which logs the build, as well
+      as scan-build tool, which logs the build and runs the clang static analyzer on
+      it.
+      '';
+    homepage = "https://github.com/rizsotto/scan-build";
+    license = [ licenses.ncsa ];
+    maintainers = [ maintainers.dwarfmaster ];
+  };
+}
