a786aaca4037b8965c6d09b1232cd2160a38687b
DwarfMaster <luc@dwarfmaster.net>
2022-09-06 21:30:47 +0200

    Un-hardcode xmobar color scheme

--- profiles/hm/interface/xmonad/default.nix
+++ profiles/hm/interface/xmonad/default.nix
@@ -9,7 +9,12 @@ let
     };
   };
 
-  xmobarrc = pkgs.writeText "xmobarrc" (builtins.readFile ./xmobarrc);
+  xmobarrc = pkgs.substituteAll {
+    src = ./xmobarrc;
+    inherit (config.colorScheme.colors)
+      base00 base01 base02 base03 base04 base05 base06 base07
+      base08 base09 base0A base0B base0C base0D base0E base0F;
+  };
 
   xmonad-compiled =
     pkgs.writers.writeHaskell
