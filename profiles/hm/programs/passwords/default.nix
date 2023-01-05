eddd8fad59d93c997633c027e61b5a7e1e26a1fc
DwarfMaster <luc@dwarfmaster.net>
2023-01-05 23:22:37 +0100

    native-messagin-host file for keepassxc is created automatically

--- profiles/hm/programs/passwords/default.nix
+++ profiles/hm/programs/passwords/default.nix
@@ -15,13 +15,4 @@
   programs.firefox.extensions = [
     pkgs.nur.repos.rycee.firefox-addons.keepassxc-browser
   ];
-  home.file.".mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".text = builtins.toJSON {
-    allowed_extensions = [
-      "keepassxc-browser@keepassxc.org"
-    ];
-    description = "KeePassXC integration with native messaging support";
-    name = "org.keepassxc.keepassxc_browser";
-    path = "${pkgs.keepassxc}/bin/keepassxc-proxy";
-    type = "stdio";
-  };
 }
