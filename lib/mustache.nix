09651365ada906b9ad5079c7e672210dc0677301
DwarfMaster <luc@dwarfmaster.net>
2023-01-08 10:46:29 +0100

    Add lib function to apply mustache to a directory

--- lib/mustache.nix
+++ lib/mustache.nix
@@ -12,4 +12,18 @@
     pkgs.runCommandLocal name {} ''
       ${pkgs.mustache-go}/bin/mustache ${json} ${template} > $out
     '';
+
+  renderDir = name: templateDir: vars: let
+    json = pkgs.writeText "${name}.json" (builtins.toJSON vars);
+  in
+    pkgs.runCommandLocal name {} ''
+      mkdir -p $out
+      dir=$(realpath ${templateDir})
+      find "$dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
+        rel=$(realpath --relative-to="$dir" "$file")
+        reldir=$(dirname "$rel")
+        mkdir -p "$out/$reldir"
+        ${pkgs.mustache-go}/bin/mustache ${json} $file > "$out/$rel"
+      done
+    '';
 }
