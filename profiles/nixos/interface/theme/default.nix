4b4ca0e38424a2600b418d6761fdcfcdf9c442d9
DwarfMaster <luc@dwarfmaster.net>
2023-02-02 01:45:19 +0100

    Download base16 automatically from tinted-theming schemes

--- profiles/nixos/interface/theme/default.nix
+++ profiles/nixos/interface/theme/default.nix
@@ -6,24 +6,13 @@ let
     package = nerdfonts;
     name = "FiraCode Nerd Font Mono";
   };
-  colors = {
-    base00 = "231e18";
-    base01 = "302b25";
-    base02 = "48413a";
-    base03 = "9d8b70";
-    base04 = "b4a490";
-    base05 = "cabcb1";
-    base06 = "d7c8bc";
-    base07 = "e4d4c8";
-    base08 = "d35c5c";
-    base09 = "ca7f32";
-    base0A = "e0ac16";
-    base0B = "b7ba53";
-    base0C = "6eb958";
-    base0D = "88a4d3";
-    base0E = "bb90e2";
-    base0F = "b49368";
+  schemes = pkgs.fetchFromGitHub {
+    owner = "tinted-theming";
+    repo = "base16-schemes";
+    rev = "cf6bc892a24af19e11383adedc6ce7901f133ea7";
+    sha256 = "sha256-U9pfie3qABp5sTr3M9ga/jX8C807FeiXlmEZnC4ZM58=";
   };
+  colors = config.lib.stylix.colors;
   bg = pkgs.runCommand "bg.png" {} ''
     cp ${./bg.svg} bg.svg
     sed -i 's/#202330/#${colors.base00}/' bg.svg
@@ -56,5 +45,6 @@ in {
       monospace = font;
     };
     image = "${bg}";
+    base16Scheme = "${schemes}/woodland.yaml";
   };
 }
