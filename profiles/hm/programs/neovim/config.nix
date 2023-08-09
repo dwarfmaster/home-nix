12c79741a6ece91ea940b39a7f75327bf95b15e3
DwarfMaster <luc@dwarfmaster.net>
2023-08-09 15:39:41 +0200

    base16-nvim is now packaged in nixvim

--- profiles/hm/programs/neovim/config.nix
+++ profiles/hm/programs/neovim/config.nix
@@ -6,8 +6,6 @@
 }: let
   tab = 2;
 in {
-  stylix.targets.nixvim.enable = true;
-
   programs.nixvim = {
     profiles.telescope.enable = true;
     profiles.treesitter.enable = true;
@@ -64,6 +62,30 @@ in {
     plugins.lualine = {
       enable = true;
       theme = "base16";
+    };
+    colorschemes.base16 = let
+      colors = config.lib.stylix.colors;
+    in {
+      enable = true;
+      colorscheme = colors.scheme-slug;
+      customColorScheme = {
+        base00 = "#${colors.base00}";
+        base01 = "#${colors.base01}";
+        base02 = "#${colors.base02}";
+        base03 = "#${colors.base03}";
+        base04 = "#${colors.base04}";
+        base05 = "#${colors.base05}";
+        base06 = "#${colors.base06}";
+        base07 = "#${colors.base07}";
+        base08 = "#${colors.base08}";
+        base09 = "#${colors.base09}";
+        base0A = "#${colors.base0A}";
+        base0B = "#${colors.base0B}";
+        base0C = "#${colors.base0C}";
+        base0D = "#${colors.base0D}";
+        base0E = "#${colors.base0E}";
+        base0F = "#${colors.base0F}";
+      };
     };
 
     # __      ___    _    _       _
