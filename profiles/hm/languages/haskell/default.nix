ff80c537f43f827ef48d3bf2ef0657c1f705e12c
DwarfMaster <luc@dwarfmaster.net>
2023-11-15 18:19:15 +0100

    Add shakespeare plugin for nvim

--- profiles/hm/languages/haskell/default.nix
+++ profiles/hm/languages/haskell/default.nix
@@ -1,8 +1,17 @@
 {
-  config,
   pkgs,
   ...
-}: {
+}: let
+  vim-shakespeare = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-syntax-shakespeare";
+    src = pkgs.fetchFromGitHub {
+      owner = "pbrisbin";
+      repo = "vim-syntax-shakespeare";
+      rev = "2f4f61eae55b8f1319ce3a086baf9b5ab57743f3";
+      sha256 = "0h79c3shzf08g7mckc7438vhfmxvzz2amzias92g5yn1xcj9gl5i";
+    };
+  };
+in {
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
@@ -35,5 +44,10 @@
 
   programs.nixvim = {
     plugins.lsp.servers.hls.enable = true;
+    extraPlugins = [ vim-shakespeare ];
+    globals = {
+      hamlet_prevent_invalid_nesting = 0;
+      hamlet_highlight_trailing_space = 0;
+    };
   };
 }
