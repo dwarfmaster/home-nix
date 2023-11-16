9a5ae5235b8a9e0079f7769ce7039630e0b9111e
DwarfMaster <luc@dwarfmaster.net>
2023-11-16 22:04:33 +0100

    Setup coqtail in neovim

--- profiles/hm/languages/coq/default.nix
+++ profiles/hm/languages/coq/default.nix
@@ -14,6 +14,42 @@ in {
   };
   programs.doom-emacs.config.initModules.lang = ["coq"];
 
+  # Neovim
+  programs.nixvim = {
+    extraPlugins = [ pkgs.vimPlugins.Coqtail ];
+    globals = {
+      coqtail_nomap = 1;
+      coqtail_coq_path = "${config.programs.coq.package}/bin";
+      coqtail_noindent_comment = 1;
+      coqtail_auto_set_proof_diffs = "off";
+    };
+    files."ftplugin/coq.lua" = {
+      keymaps = let
+        mk = key: action: desc: {
+          mode = "n";
+          inherit key;
+          action = ":${action}<cr>";
+          options.desc = desc;
+        };
+      in [
+        (mk "<leader> ps" "CoqStart" "Start Coq")
+        (mk "<leader> pq" "CoqStop" "Stop Coq")
+        (mk "<leader> pi" "CoqInterrupt" "Interrupt Coq")
+        (mk "<leader> j" "CoqNext" "Check next sentence")
+        (mk "<leader> k" "CoqUndo" "Undo last sentense")
+        (mk "<leader> ." "CoqToLine" "Check to current line")
+        (mk "<leader> gg" "CoqToTop" "Rewind to the beggining of file")
+        (mk "<leader> gc" "CoqJumpToEnd" "Move cursor the end of checked")
+        (mk "<leader> ge" "CoqJumpToError" "Move cursor to error")
+        (mk "<leader> R" "CoqRestorePanels" "Restore panels")
+        (mk "]g" "CoqGotoGoalNext" "Scroll to next goal")
+        (mk "]G" "CoqGotoGoalNext!" "Scroll to end of next goal")
+        (mk "[g" "CoqGotoGoalPrev" "Scroll to prev goal")
+        (mk "[G" "CoqGotoGoalPrev!" "Scroll to end of prev goal")
+      ];
+    };
+  };
+
   # When installing coq through opam, it needs gmp and zlib
   home.packages = [pkgs.gmp pkgs.zlib];
   pkgconfig.enable = true;
