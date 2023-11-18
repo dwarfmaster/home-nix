7f1c972a05264850727d4d404b871a481788e876
DwarfMaster <luc@dwarfmaster.net>
2023-11-18 10:58:05 +0100

    Improve nvim-cmp bindings

--- profiles/nixvim/cmp/default.nix
+++ profiles/nixvim/cmp/default.nix
@@ -3,7 +3,8 @@
   pkgs,
   ...
 }: {
-  # TODO more complete setup
+  # TODO luasnip
+  # TODO cmdline still not working
   extraPlugins = [
     pkgs.vimPlugins.luasnip
     pkgs.vimPlugins.cmp_luasnip
@@ -11,7 +12,7 @@
   ];
   plugins.nvim-cmp = {
     enable = true;
-    mappingPresets = ["insert" "cmdline"];
+    # mappingPresets = ["cmdline"];
     snippet.expand = "luasnip";
     preselect = "None";
     # TODO
@@ -27,57 +28,76 @@
       {name = "path";}
       {name = "luasnip";}
     ];
-    mapping = {
-      "<Tab>" = {
-        modes = ["i" "s"];
-        action = ''
-          function(fallback)
-            if cmp.visible then
-              cmp.select_next_item()
-            elseif luasnip.expandable() then
-              luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif check_backspace() then
-              fallback()
+    mapping = let
+      tabAction = ''
+        function(fallback)
+          if cmp.visible() then
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
             else
-              fallback()
+              cmp.select_next_item()
+            end
+          -- TODO setup luasnip
+          -- elseif luasnip.expandable() then
+          --   luasnip.expand()
+          -- elseif luasnip.expand_or_jumpable() then
+          --   luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end
+      '';
+    in {
+      "<Tab>" = ''cmp.mapping({
+          i = ${tabAction},
+          s = ${tabAction},
+          c = function(_)
+            if cmp.visible() then
+              if #cmp.get_entries() == 1 then
+                cmp.confirm({ select = true })
+              else
+                cmp.select_next_item()
+              end
+            else
+              cmp.complete()
+              if #cmp.get_entries() == 1 then
+                cmp.config({ select = true })
+              end
             end
           end
-        '';
-      };
+        })
+      '';
       "S-<Tab>" = {
         modes = ["i" "s"];
         action = ''
           function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
+            -- elseif luasnip.jumpable(-1) then
+            --   luasnip.jump(-1)
             else
               fallback()
             end
           end
         '';
       };
-      "<Down>" = {
-        modes = ["i"];
-        action = ''
-          function(fallback)
-            fallback()
-          end
-        '';
-      };
-      "<Up>" = {
-        modes = ["i"];
-        action = ''
-          function(fallback)
-            fallback()
-          end
-        '';
-      };
+      # If entry is selected, return selects it, otherwise inserts a new line
+      "<CR>" = ''
+        cmp.mapping({
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              fallback()
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        })
+      '';
     };
-    # TODO improve format
     formatting = {
       fields = ["kind" "abbr" "menu"];
       format = ''
@@ -96,4 +116,17 @@
       '';
     };
   };
+  extraConfigLuaPost = ''
+    do
+      local cmp = require('cmp')
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'nvim_lua' },
+          { name = 'cmdline' },
+          { name = 'path' },
+        },
+      })
+    end
+  '';
 }
