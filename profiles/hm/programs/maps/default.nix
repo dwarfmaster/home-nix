f01f2dfe189842f6de12213bd259cd41d02a09c1
DwarfMaster <luc@dwarfmaster.net>
2023-12-22 17:24:57 +0100

    Setup viking

--- profiles/hm/programs/maps/default.nix
+++ profiles/hm/programs/maps/default.nix
@@ -7,6 +7,12 @@
     inherit
       (pkgs)
       viking # GPS traces editor
+      qgis
       ;
   };
+
+  home.persistence."/persists/luc".directories = [
+    ".config/viking"
+    ".cache/viking"
+  ];
 }
