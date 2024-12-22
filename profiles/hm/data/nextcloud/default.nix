7a6831678a2cf34e54158003e0ffe054880a2c71
DwarfMaster <luc@dwarfmaster.net>
2024-12-22 23:15:03 +0100

    Setup vdirsyncer for usage with korrvigs

--- profiles/hm/data/nextcloud/default.nix
+++ profiles/hm/data/nextcloud/default.nix
@@ -9,21 +9,20 @@
     startInBackground = true;
   };
 
+    # [pair nextcloud_contacts]
+    # a = "contacts_local"
+    # b = "nextcloud_contacts_remote"
+    # collections = [ [ "nextcloud", "nextcloud", "contacts" ] ]
+    #
+    # [storage contacts_local]
+    # type = "filesystem"
+    # path = "~/data/korrvigs-temp/calsync/korrvigs"
+    # fileext = ".vcf"
+
   # Sync calendar and contacts
   xdg.configFile."vdirsyncer/config".text = ''
     [general]
     status_path = "${config.xdg.cacheHome}/vdirsyncer/status/"
-
-    [pair nextcloud_contacts]
-    a = "contacts_local"
-    b = "nextcloud_contacts_remote"
-    collections = [ [ "nextcloud", "nextcloud", "contacts" ] ]
-    conflict_resolution = "a wins"
-
-    [storage contacts_local]
-    type = "filesystem"
-    path = "~/data/annex/contacts/"
-    fileext = ".vcf"
 
     [storage nextcloud_contacts_remote]
     type = "carddav"
@@ -31,16 +30,23 @@
     username = "luc"
     password.fetch = [ "command", "${pkgs.pass}/bin/pass", "dwarfmaster.net/nextcloud/luc" ]
 
-    [pair nextcloud_calendars]
+    [pair nextcloud_calendars_pull]
     a = "calendars_local"
     b = "nextcloud_calendars_remote"
-    collections = [ "from a", "from b" ]
+    collections = [ "dedukteam", "jeux", "people", "personal", "sant", "sport", "clearsy", "dance" ]
+    conflict_resolution = "b wins"
+    metadata = [ "color" ]
+
+    [pair nextcloud_calendars_push]
+    a = "calendars_local"
+    b = "nextcloud_calendars_remote"
+    collections = [ "dedukteam", "jeux", "people", "personal", "sant", "sport", "clearsy", "dance" ]
     conflict_resolution = "a wins"
     metadata = [ "color" ]
 
     [storage calendars_local]
     type = "filesystem"
-    path = "~/data/annex/calendars/"
+    path = "~/data/korrvigs-temp/calsync/korrvigs/events"
     fileext = ".ics"
 
     [storage nextcloud_calendars_remote]
@@ -50,4 +56,7 @@
     password.fetch = [ "command", "${pkgs.pass}/bin/pass", "dwarfmaster.net/nextcloud/luc" ]
   '';
   home.packages = [pkgs.vdirsyncer];
+  home.persistence."/persists/luc".directories = [
+    ".cache/vdirsyncer"
+  ];
 }
